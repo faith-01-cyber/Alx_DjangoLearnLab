d38259b029cd7a71b3b1ae90d82d6aeb99cf7cff
Faith Kemboi <kemboifaith941@gmail.com>
2026-01-24 11:09:58 +0300

    Add explicit Library import for class-based view checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 
 # Function-based view: list all books

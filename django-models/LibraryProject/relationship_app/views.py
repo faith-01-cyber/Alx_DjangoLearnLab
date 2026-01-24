1ee02f12e4c80bda5ea9ef886207bd1656de43ae
Faith Kemboi <kemboifaith941@gmail.com>
2026-01-24 11:01:08 +0300

    Fix views: function-based and class-based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from relationship_app.models import Book, Library
+
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+
+# Class-based view: library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+

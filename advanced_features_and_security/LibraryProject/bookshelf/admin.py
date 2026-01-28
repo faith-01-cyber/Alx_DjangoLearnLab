db1052f06c6219113cb2517c599f40e2a0982325
Faith Kemboi <kemboifaith941@gmail.com>
2026-01-28 17:24:39 +0300

    Register CustomUser with CustomUserAdmin in admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year',)
-    search_fields = ('title', 'author')
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    list_display = ('username', 'email', 'is_staff', 'date_of_birth')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 

7d021cdf3cd3cf041a891efbb083f2670dbd887a
Faith Kemboi <kemboifaith941@gmail.com>
2026-01-28 11:14:43 +0300

    Add function-based and class-based views with URL configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,39 +1,18 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from .models import Book
-from .forms import BookForm  # Make sure you have a simple ModelForm for Book
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
-# Add a new book
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('list_books')
-    else:
-        form = BookForm()
-    return render(request, "relationship_app/book_form.html", {"form": form})
+from .models import Book, Library
 
-# Edit a book
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('list_books')
-    else:
-        form = BookForm(instance=book)
-    return render(request, "relationship_app/book_form.html", {"form": form})
 
-# Delete a book
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('list_books')
-    return render(request, "relationship_app/book_confirm_delete.html", {"book": book})
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+# Class-based view: library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
 

93a31b76717d830ad3d9ccdd4d926bc131614f9f
Faith Kemboi <kemboifaith941@gmail.com>
2026-01-29 10:49:20 +0300

    Add book_list view with permission_required decorator

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.view_books),
+    path('view/', views.book_list),
     path('create/', views.create_book),
     path('edit/', views.edit_book),
     path('delete/', views.delete_book),

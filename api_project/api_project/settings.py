27d6acc7d78c5977a3978941b63e56ceb3af5b93
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-03 17:15:48 +0300

    Fix API project structure and app configuration

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)o+)x!@lkxz(axv#*ic4g6^x#x(on9c%80j%o4)b)h7lp8o)wq'
+SECRET_KEY = 'django-insecure-8u9qes^)2sfuekb1f9$r!&)r+7^!sew#$m7wfzfzx)9rsc+&!x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'rest_framework',
-    'api',
+    'api_app',
 ]
 
 MIDDLEWARE = [

138110593126498dc8b5f0adcec063d6409ad499
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-04 12:01:52 +0300

    Use generics.ListAPIView for BookList view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(ListAPIView):
+class BookList(List.APIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 

997130a713597ce643af1cf3873d114a96008605
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-09 18:12:47 +0300

    Update Book model and add rest_framework to settings

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import datetime
+
+class Author(models.Model):
+    """Represents an author."""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
+    """Represents a book."""
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.publication_year > datetime.now().year:
+            raise ValidationError("Publication year cannot be in the future.")
 
     def __str__(self):
         return self.title
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [

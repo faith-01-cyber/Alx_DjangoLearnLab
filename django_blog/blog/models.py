42831e191eaae086b8916d5dfef1f2176972f7c4
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-16 16:38:39 +0300

    Implemented tagging functionality using django-taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,20 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Post model
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)  # renamed from published_date
-    updated_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.pk})
 
 # Comment model
 class Comment(models.Model):

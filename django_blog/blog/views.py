722e58cdd96ff05b8664ca4264fac48c1b54eaa1
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-16 13:26:19 +0300

    Add Comment CRUD with LoginRequiredMixin and UserPassesTestMixin

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,60 +1,87 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
+# ======================
 # Post Views
+# ======================
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-created_at']
+    ordering = ['-created_at']  # updated to match your Post model
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post-list')
 
-class PostUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post-list')
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post-list')
 
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+# ======================
 # Comment Views
-class CommentCreateView(CreateView):
+# ======================
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        form.instance.post_id = self.kwargs['post_id']
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         return super().form_valid(form)
 
     def get_success_url(self):
-        return self.object.post.get_absolute_url()
+        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['post_id']})
 
-class CommentUpdateView(UpdateView):
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
 
-class CommentDeleteView(DeleteView):
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.id})
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
 
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
     def get_success_url(self):
-        return self.object.post.get_absolute_url()
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.id})
